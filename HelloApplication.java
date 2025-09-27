836b6ba06aa01bc6fa49b49ca1e9f46958d923b3
cristian.ledesma@uabc.edu.mx <cristian.ledesma@uabc.edu.mx>
2025-09-26 18:15:06 -0700

    Actualización de clases

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,18 +1,17 @@
 package com.example.juego3dados;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        ManejadorJuego manejadorJuego = new ManejadorJuego();
+        Tablero tablero = new Tablero(manejadorJuego);
+
+        Scene scene = new Scene(tablero, 700, 500);
+        stage.setTitle("Juego 3 Dados");
         stage.setScene(scene);
         stage.show();
     }
